4bc09bd70943926e1e32c86f43abcb27908011a5
Yevhen Boiko <yevhenii.boiko@atorusresearch.com>
2024-02-13 19:14:18 +0000

    Add configure_yaml function

---
+++ R/configure_yaml.R
@@ -0,0 +1,51 @@
+#' Replace placeholders in imported YAML config with values supplied by user
+#'
+#' @param yaml_file_obj a nested list representing template YAML config
+#' @param file_path full path to R file
+#' @param user_tag String that describes what kind of job will be scheduled to run
+#' @param cpu_limit maximum number of cores available for Kubernetes container
+#' @param memory_limit Maximum amount of RAM available for Kubernetes container
+#'
+#' @return A nested named list, yaml_file_obj, with placeholders replaced by actual values
+#' @export
+#'
+#' @examples
+#' config <- abba::load_yaml_template()
+#' config <- configure_yaml(config, file_path="/path/to/file.R", user_tag="test program")
+configure_yaml <- function(yaml_file_obj,
+                           file_path='',
+                           user_tag='',
+                           cpu_limit= 1L,
+                           memory_limit='512M'){
+
+  program_name <- unlist(strsplit(basename(file_path), '.', fixed = TRUE))[1]
+  generate_name <- paste0(program_name, '-', uuid::UUIDgenerate())
+  # temporary plug before figuring out where to get service user identity
+  service_user <- Sys.info()[["user"]]
+
+
+  # a function that would try to replace all possible keywords inside the target string
+  replace_func <- function(x){
+    x <- sub("JOB_NAME", program_name, x)
+    x <- sub("GENERATE_NAME", generate_name, x)
+    x <- sub("USER_TAG", user_tag, x)
+    x <- sub("PROGRAM_FULL_PATH", file_path, x)
+    x <- sub("PROGRAM_BASE_NAME", program_name, x)
+    x <- sub("SERVICE_USER", service_user, x)
+    x <- sub("CPU_LIMIT", as.character(cpu_limit), x)
+    x <- sub("MEMORY_LIMIT", memory_limit, x)
+    return(x)
+  }
+
+  # recursively walk the yaml and replace all placeholders with actual values
+  recursive_replace <- function(l){
+    lapply(l, function(x) if(is.list(x)) recursive_replace(x)
+                          else if(is.character(x)) replace_func(x)
+                          else x)
+  }
+
+  yaml_file_obj <- recursive_replace(yaml_file_obj)
+
+  return(yaml_file_obj)
+
+}
---
+++ tests/testthat/test-configure_yaml.R
@@ -0,0 +1,20 @@
+test_that("YAML fields are properly updated by configure_yaml", {
+  yaml_file <- load_yaml_template()
+  yaml_file_configured <- configure_yaml(yaml_file,
+                                         file_path="/tst/path/test.R",
+                                         user_tag='user_tag',
+                                         cpu_limit= 2L,
+                                         memory_limit='512M')
+  actual <- c(yaml_file_configured$metadata$name,
+              yaml_file_configured$spec$template$spec$containers[[1]]$args[[2]],
+              yaml_file_configured$spec$template$spec$containers[[1]]$resources$limits$cpu,
+              yaml_file_configured$spec$template$spec$containers[[1]]$resources$limits$memory)
+
+  expected <- c("test",
+                paste0("cd ~ && R --slave --no-save --no-restore -f ", "/tst/path/test.R"),
+                "2",
+                "512M")
+
+  expect_equal(actual, expected)
+
+})
