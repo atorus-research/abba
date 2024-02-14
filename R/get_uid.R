d7466cf5aeeb33331f8cec436184d9f9854732b6
Yevhen Boiko <yevhenii.boiko@atorusresearch.com>
2024-02-14 13:47:56 +0000

    Add a function to get uid/gid of a current user

---
+++ R/get_uid.R
@@ -0,0 +1,15 @@
+#' Get numerical User ID/ Group ID on linux
+#'
+#' @return a list of uid and gid number for user executing this function
+#' @export
+#' @noRd
+#' @examples
+#' ids <- get_guid()
+get_guid <- function(){
+  output <- system("id", intern=TRUE)
+
+  uid <- stringr::str_extract(output, regex("(?<=uid\\=)\\d+(?=\\()"))
+  gid <- stringr::str_extract(output, regex("(?<=gid\\=)\\d+(?=\\()"))
+
+  return(list(uid=uid, gid=gid))
+}
