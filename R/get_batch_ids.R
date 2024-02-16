ed0c075ff21766ab265481b9b2ae76ee3ccdab1f
Yevhen Boiko <yevhenii.boiko@atorusresearch.com>
2024-02-16 13:34:25 +0000

    Add a function to retrive job ids belonging to a batch

---
+++ R/get_batch_ids.R
@@ -0,0 +1,24 @@
+#' Get IDs for all jobs marked with a given batch id
+#'
+#' @param group_batch_id batch ID tag that was used to mark submitted jobs
+#' @noRd
+#' @return a character vector of job IDs
+#'
+get_batch_ids <- function(group_batch_id){
+
+  # get list of job IDs with a given batch group
+  output <- system2(command="kubectl",
+                    args=c("get", "pods", "-n" ,"rstudio", paste0("-l batch-group=", group_batch_id)),
+                    stdout=TRUE, stderr=TRUE)
+
+  # if there are no jobs with a given batch id, command will return a single string.
+  # return an empty list in this case
+  if (length(output)==1){return(list())}
+
+  # remove header from output
+  jobids <- utils::tail(output, 2)
+  jobids_list <- sapply(jobids,
+                        function(x) stringr::str_extract(x,  stringr::regex("^[\\d\\w\\-]+")),
+                        USE.NAMES=FALSE)
+  return(jobids_list)
+}
