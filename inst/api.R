0d9e3fb9d3517cfea609a9d85e6c33d4fd84fc21
mike.stackhouse <Mike.Stackhouse@atorusresearch.com>
2024-02-21 19:12:48 +0000

    Get a job actually functional from the API

--- inst/api.R
+++ inst/api.R
@@ -5,15 +5,24 @@ options(
 
 #* Submit a and monitor a job on Kubernetes
 #' @param file_path Full path to R file
-#' @param batch_group_id Group ID for batch processing
-#' @param user_tag Optional; a string that describes what kind of job will be scheduled to run
 #' @param cpu_limit Maximum number of cores available for Kubernetes container
 #' @param memory_limit Maximum amount of RAM available for Kubernetes container
-#' @param poll_interval_seconds Time interval for polling job status in seconds
 #' @param timeout_seconds Total time to wait before timeout in seconds
 #* @post /submit
-function(file_path, batch_group_id='', user_tag='', cpu_limit=1L,
-         memory_limit=512L, poll_interval_seconds = 3L, timeout_seconds = 600L) {
+function(file_path, cpu_limit="1", memory_limit="512", poll_interval_seconds = "3",
+         timeout_seconds = "14400") {
+
+
+  # All args come in as character so make sure they're integers
+  cpu_limit <- as.integer(cpu_limit)
+  memory_limit <- as.integer(memory_limit)
+  poll_interval_seconds <- as.integer(poll_interval_seconds)
+  timeout_seconds <- as.integer(timeout_seconds)
+
+  if (is.na(cpu_limit)) stop("cpu_limit must be provided as an integer")
+  if (is.na(memory_limit)) stop("memory_limit must be provided as an integer")
+  if (is.na(poll_interval_seconds)) stop("poll_interval_seconds must be provided as an integer")
+  if (is.na(timeout_seconds)) stop("timeout_seconds must be provided as an integer")
 
   if (as.numeric(cpu_limit) > getOption('abba.cpu.limit')) {
     stop(sprintf(
@@ -21,28 +30,22 @@ function(file_path, batch_group_id='', user_tag='', cpu_limit=1L,
     ))
   }
 
-  print(memory_limit)
-  print(getOption('abba.memory.limit'))
-
   if (as.numeric(memory_limit) > getOption('abba.memory.limit')) {
     stop(sprintf(
       "The supplied memory limit is greater that the maximum limit of %s M", getOption('abba.memory.limit')
     ))
   }
 
-  return(
-    list(file_path, batch_group_id, user_tag, cpu_limit,
-    memory_limit, poll_interval_seconds, timeout_seconds)
+  memory_limit_c <- paste0(memory_limit, "M")
+
+  result <- submit_job_and_poll(
+    file_path,
+    cpu_limit=cpu_limit,
+    memory_limit=memory_limit_c,
+    poll_interval_seconds = poll_interval_seconds,
+    timeout_seconds = timeout_seconds
   )
 
-  # submit_job_and_poll(
-  #   file_path,
-  #   batch_group_id=batch_group_id,
-  #   user_tag=user_tag,
-  #   cpu_limit=cpu_limit,
-  #   memory_limit=memory_limit,
-  #   poll_interval_seconds = poll_interval_seconds,
-  #   timeout_seconds = timeout_seconds
-  # )
+  print(result)
 }
 
