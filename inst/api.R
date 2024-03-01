d4fcb9773f8c7b13ddb957bdbe4bf22561f1cce8
Yevhenii Boiko <Yevhenii.Boiko@atorusresearch.com>
2024-03-01 20:56:46 +0000

    Apply suggestions from code review

--- inst/api.R
+++ inst/api.R
@@ -3,7 +3,8 @@ options(
   abba.cpu.limit = 2,
   abba.lower.memory.limit='128M',
   abba.memory.limit='1G',
-  abba.api.address="http://127.0.0.1:5794"
+  abba.api.address="http://127.0.0.1:5794,
+  "plumber.port" = 5794
   )
 
 #* Submit a job on Kubernetes
