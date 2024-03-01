6e6d1b892680d5fd46d79cfdf7196cffdb2a1894
Yevhen Boiko <yevhenii.boiko@atorusresearch.com>
2024-03-01 20:48:18 +0000

    Add function to submit a job and retrieve its log when it is done running

--- R/api_requests.R
+++ R/api_requests.R
@@ -90,6 +90,71 @@ send_submit_job_and_watch <-
     return(httr2::resp_body_json(resp))
   }
 
+#' Send job and poll for status. This function sends multiple requests so it won't
+#' time out on heavy jobs
+#'
+#' @param file_path Full path to R file
+#' @param batch_group_id Group ID for batch processing
+#' @param user_tag Optional; a string that describes what kind of job will be scheduled to run
+#' @param cpu_limit Maximum number of cores available for Kubernetes container
+#' @param memory_limit Maximum amount of RAM available for Kubernetes container
+#' @param container list that contains container name and image name
+#' @param mounts Specifically formatted list with information bout volumes that container would have access to during the run
+#' @param timeout_seconds Total time to wait before timeout in seconds
+#' @param poll_interval_seconds Total time to wait before timeout in seconds
+#'
+#' @return list with 2 attributes: job_id for submitted job`s id, and its logs
+#' @export
+#'
+#' @examples \dontrun{
+#' response <- send_submit_job_and_watch('/path/to/R/program.R', batch_group_id='SDTM')}
+send_submit_job_and_wait_for_log <-
+  function(file_path,
+           batch_group_id='',
+           user_tag='',
+           cpu_limit=1L,
+           memory_limit='512M',
+           container='',
+           mounts='',
+           poll_interval_seconds = 3,
+           timeout_seconds = 600,
+           api_address=getOption("abba.api.address")) {
+
+    # submit the job
+    job_id <- send_submit_job(file_path=file_path,
+                              batch_group_id=batch_group_id,
+                              user_tag=user_tag,
+                              cpu_limit=cpu_limit,
+                              memory_limit=memory_limit,
+                              mounts=mounts,
+                              container=container)
+
+    start_time <- Sys.time()
+
+    # Poll for job status in the specified batch group
+    while (difftime(Sys.time(), start_time, units = "secs") <= timeout_seconds) {
+
+      # Get the status
+      job_details <- send_get_batch_status(job_id$batch_id, api_address=api_address)
+
+      # Get the names of the outer list in job_details
+      status_names <- names(job_details)
+
+      # Check if neither "Pending" nor "Running" is a name in job_details
+      if (!"Pending" %in% status_names && !"Running" %in% status_names) {
+        break # Break if no "Pending" or "Running" in the names of job_details
+      }
+
+      # Wait for the specified interval before polling again
+      Sys.sleep(poll_interval_seconds)
+    }
+
+    # get logs after job is no long in pending/running stage
+    logs <- send_get_job_log(job_id$job_id, api_address = api_address)
+
+    # return response as a list
+    return(list(job=job_id, logs=logs))
+  }
 
 #' Send GET request to get logs of specified Jobs
 #'
