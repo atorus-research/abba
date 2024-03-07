f9ed22d50e1cddd9c6488b244729d8acb5b6227c
Yevhen Boiko <yevhenii.boiko@atorusresearch.com>
2024-03-07 19:02:38 +0000

    Update tests that validate add_api_key_to_header function

--- tests/testthat/test-add_api_key_to_header.R
+++ tests/testthat/test-add_api_key_to_header.R
@@ -17,6 +17,6 @@ test_that("function errors when api_key is not NULL or character", {
 
 test_that("Function adds proper header when api_key is supplied", {
   req <- httr2::request("base_url") %>% add_api_key_to_header(api_key='test_key')
-  actual <- list(Authorization='Key test_key')
-  expect_equal(req$headers, actual)
+  actual <- 'Key test_key'
+  expect_equal(req$headers$Authorization, actual)
   })
