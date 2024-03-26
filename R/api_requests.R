02f0a6f93d7c01b9921f55d1dfa291d2cdc26837
Yevhen Boiko <yevhenii.boiko@atorusresearch.com>
2024-03-26 18:01:19 +0000

    Make k8s functions compatible with batch submission function

--- R/api_requests.R
+++ R/api_requests.R
@@ -32,7 +32,8 @@ abba_submit_job <-
            auto_mount_home=FALSE,
            home_nfs_ip_address=getOption('abba.home.nfs.ip.address'),
            api_address=Sys.getenv("ABBA_API_ADDRESS"),
-           api_key=Sys.getenv("ABBA_API_KEY")) {
+           api_key=Sys.getenv("ABBA_API_KEY"),
+           ...) {
 
     # address for a submit_job_and_watch endpoint
     req <- httr2::request(paste(api_address, 'submit-job', sep='/')) %>%
@@ -167,7 +168,8 @@ abba_wait_for_job_log <-
            poll_interval_seconds = 3,
            timeout_seconds = 600,
            api_address=Sys.getenv("ABBA_API_ADDRESS"),
-           api_key=Sys.getenv("ABBA_API_KEY")) {
+           api_key=Sys.getenv("ABBA_API_KEY"),
+           ...) {
 
     start_time <- Sys.time()
 
