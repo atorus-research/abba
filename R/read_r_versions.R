76aa14982058b00973cf6ce1f120279be62d9737
Yevhen Boiko <yevhenii.boiko@atorusresearch.com>
2024-05-21 16:01:38 +0000

    Add function for selecting rscript version

--- R/read_r_versions.R
+++ R/read_r_versions.R
@@ -1,8 +1,8 @@
 read_r_versions <- function(){
 
   # read data from r-versions json
-  prep <- read.delim("/etc/rstudio/r-versions", sep=":", header=FALSE,
-                       comment.char="#", strip.white=TRUE)
+  prep <- utils::read.delim("/etc/rstudio/r-versions", sep=":", header=FALSE,
+                            comment.char="#", strip.white=TRUE)
   # calculate values for ID column(need for transforming data)
   id_lines <- Filter(function(x) !startsWith(x, '#'), readLines("/etc/rstudio/r-versions"))
   ids <- cumsum(id_lines == "")[id_lines!=""]
@@ -17,10 +17,11 @@ read_r_versions <- function(){
 
   # reshape data frame
   result <- as.data.frame(tidyr::pivot_wider(prep,
-                                             names_from = Property,
-                                             values_from = Value))
+                                             names_from = "Property",
+                                             values_from = "Value"))
   # add 'bin/R' to path
   result$Path <- sapply(result$Path, function(x) file.path(x, 'bin', 'R'))
+  result$RScriptPath <- sapply(result$Path, function(x) file.path(x, 'bin', 'Rscript'))
   # remove any entries which don't actually exist
   result <- result[file.exists(result$Path),]
 
@@ -31,7 +32,7 @@ select_r_version <- function(r_version){
   # default to standard workbench R version if none specified by user
   if (is.null(r_version) || is.na(r_version) || r_version == ""){
     return(file.path(R.home("bin"), "R"))
-    }
+  }
 
   # read list of r versions available for workbench
   av <- read_r_versions()
@@ -46,6 +47,29 @@ select_r_version <- function(r_version){
   else{stop(paste(
     sprintf("R version '%s' not found. Cannot submit job.", r_version),
     "List of available R versions:\n",
-    paste(capture.output(print(av, row.names = FALSE)), collapse="\n")))}
+    paste(utils::capture.output(print(av, row.names = FALSE)), collapse="\n")))}
+
+}
+
+select_rscript_version <- function(r_version){
+  # default to standard workbench R version if none specified by user
+  if (is.null(r_version) || is.na(r_version) || r_version == ""){
+    return(file.path(R.home("bin"), "Rscript"))
+  }
+
+  # read list of r versions available for workbench
+  av <- read_r_versions()
+  # if label is supplied, return R executable path
+  if (r_version %in% av$Label){
+    return(av[av$Label == r_version,]$RScriptPath)
+  }
+  # if exe path is supplied, return it back if it is listed in workbench r version
+  else if (r_version %in% av$RScriptPath){
+    return(r_version)
+  }
+  else{stop(paste(
+    sprintf("R version '%s' not found. Cannot submit job.", r_version),
+    "List of available R versions:\n",
+    paste(utils::capture.output(print(av, row.names = FALSE)), collapse="\n")))}
 
 }
