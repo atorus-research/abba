0c691ed0160f7de4d1f9683ad3e662d4f238fafc
yevhenii.boiko <yevhenii.boiko@atorusresearch.com>
2024-06-10 11:37:15 +0000

    Update tests for k8s functions

--- tests/testthat/test-get_job_log.R
+++ tests/testthat/test-get_job_log.R
@@ -1,21 +1,20 @@
-test_that("Supplying non-existing job id returns an error message", {
-  log <- get_k8s_job_log0("non_exst_job_id")
-  expect_equal(log, "Job non_exst_job_id not found.")
+test_that("Supplying non-existing job id produces an error", {
+  expect_error(get_k8s_job_log0("non_exst_job_id"))
 })
 
-test_that("Given a non-existing pod id, get_k8s_pod_log0 returns an error message", {
-  log <- get_k8s_pod_log0 ("non-existing-pod-id-2135")
-  expect_equal(log, "Pod non-existing-pod-id-2135 not found.")
+test_that("Given a non-existing pod id, get_k8s_pod_log0 produces an error", {
+  expect_error(get_k8s_pod_log0("non-existing-pod-id-2135"))
 })
 
 test_that("Supplying more than 1 job id returns a list with amount of elements equal to amount of supplied job ids", {
+  m <- mock('job 1 log line 1', 'job 2 log line 1')
+  stub(abba_get_k8s_job_log_local, 'system2', m, depth=2)
   log <- abba_get_k8s_job_log_local(c("job_id1", "job_id2"))
   expect_equal(length(log), 2L)
 })
 
 test_that("Given a non-existing batch id, get_batch_log returns an empty list", {
-  log <- abba_get_k8s_batch_log_local("non-existing-batch-id-2135")
-  expect_equal(log, list())
+  expect_error(abba_get_k8s_batch_log_local("non-existing-batch-id-2135"))
 })
 
 test_that("get_k8s_pod_log0 returns log for an existing pod", {
